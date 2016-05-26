ccbfc9fc246595c38dacfee4f00174a28522e8d4
Zach Clenaghan <zach.clenaghan@gmail.com>
2016-05-26 09:58:29 +0100

    Initial Commit

---
+++ stackatada-stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackatada-stack
+//
+//  Created by Zach Mcmillan-Clenaghan on 26/05/2016.
+//  Copyright © 2016 ClenTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
